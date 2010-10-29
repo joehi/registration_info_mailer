913326e265fa18323a6203ffe8ad0527fe8c952b
LeoUnglaub <leo@leo-unglaub.net>
2010-10-29 23:30:09 +0000

    git-svn-id: http://svn.lu-hosting.net/registration_info_mailer@9 3f0fd1bf-d0b2-40c0-b12f-0b57648f5385

---
+++ tags/2.0.1/system/modules/registration_info_mailer/config/database.sql
@@ -0,0 +1,33 @@
+-- **********************************************************
+-- *                                                        *
+-- * IMPORTANT NOTE                                         *
+-- *                                                        *
+-- * Do not import this file manually but use the TYPOlight *
+-- * install tool to create and maintain database tables!   *
+-- *                                                        *
+-- **********************************************************
+
+
+-- 
+-- Table `tl_page`
+-- 
+CREATE TABLE `tl_page` (
+  `rim_active` char(1) NOT NULL default '',
+  `rim_do_syslog` char(1) NOT NULL default '',
+  `rim_act_active` char(1) NOT NULL default '',
+  `rim_act_do_syslog` char(1) NOT NULL default '',
+  `rim_mail_from` varchar(255) NOT NULL default '',
+  `rim_mail_from_name` varchar(255) NOT NULL default '',
+  `rim_subject` varchar(255) NOT NULL default '',
+  `rim_mailto` blob NULL,
+  `rim_mailto_cc` blob NULL,
+  `rim_mailto_bcc` blob NULL,
+  `rim_mailtext` blob NULL,
+  `rim_act_mail_from` varchar(255) NOT NULL default '',
+  `rim_act_mail_from_name` varchar(255) NOT NULL default '',
+  `rim_act_subject` varchar(255) NOT NULL default '',
+  `rim_act_mailto` blob NULL,
+  `rim_act_mailto_cc` blob NULL,
+  `rim_act_mailto_bcc` blob NULL,
+  `rim_act_mailtext` blob NULL,
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
