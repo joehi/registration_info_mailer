efdc8c2e2cd290cef9734b05ec117c7f84a614b9
LeoUnglaub <leo@leo-unglaub.net>
2011-01-14 00:57:31 +0000

    - change the copyright to "Leo Unglaub 2011"
    git-svn-id: http://svn.lu-hosting.net/registration_info_mailer@11 3f0fd1bf-d0b2-40c0-b12f-0b57648f5385

--- trunk/system/modules/registration_info_mailer/registration_info_mailer.php
+++ trunk/system/modules/registration_info_mailer/registration_info_mailer.php
@@ -21,7 +21,7 @@
  * Software Foundation website at <http://www.gnu.org/licenses/>.
  *
  * PHP version 5
- * @copyright  LU-Hosting 2010
+ * @copyright  Leo Unglaub 2011
  * @author     Leo Unglaub <leo@leo-unglaub.net>
  * @package    registration_info_mailer
  * @license    LGPL
@@ -32,7 +32,7 @@
 /**
  * Class registration_info_mailer
  *
- * @copyright	LU-Hosting 2010
+ * @copyright	Leo Unglaub 2011
  * @author		Leo Unglaub <leo@leo-unglaub.net>
  * @package		registration_info_mailer
  * @license		LGPL
