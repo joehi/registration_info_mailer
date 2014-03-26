d140dbc4b84a2b342aaa56651cc768ef3a1ca106
Andreas Isaak <andy.jared@googlemail.com>
2014-03-26 23:38:45 +0100

    Correct language files

--- system/modules/registration_info_mailer/languages/de/tl_nc_notification.php
+++ system/modules/registration_info_mailer/languages/de/tl_nc_notification.php
@@ -4,7 +4,7 @@
  * Contao Open Source CMS
  *
  * @copyright  MEN AT WORK 2014
- * @package    maxmind
+ * @package    registration_info_mailer
  * @license    GNU/LGPL
  * @filesource
  */
@@ -14,8 +14,7 @@
  * Select lookup
  */
 $GLOBALS['TL_LANG']['tl_nc_notification']['type']['rim']                        = 'RegistrationInfoMailer';
-$GLOBALS['TL_LANG']['tl_nc_notification']['type']['member_activation_mail']     = array('Bestätigung einer Mitgliederregistrierung', '');
-$GLOBALS['TL_LANG']['tl_nc_notification']['type']['member_registration_mail']   = array('Information einer Mitgliederregistrierung', '');
-$GLOBALS['TL_LANG']['tl_nc_notification']['type']['account_activation_mail']    = array('Aktivierung eines Accounts', '');
-$GLOBALS['TL_LANG']['tl_nc_notification']['type']['account_deactivation_mail']  = array('Deaktivierung eines Accounts', '');
-
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['member_activation_mail']     = array('Confirmation of a member activation', '');
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['member_registration_mail']   = array('Information of a member registration', '');
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['account_activation_mail']    = array('Activation of an account', '');
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['account_deactivation_mail']  = array('Deactivation an account', '');
---
+++ system/modules/registration_info_mailer/languages/en/tl_nc_notification.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @copyright  MEN AT WORK 2014
+ * @package    registration_info_mailer
+ * @license    GNU/LGPL
+ * @filesource
+ */
+
+
+/**
+ * Select lookup
+ */
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['rim']                        = 'RegistrationInfoMailer';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['member_activation_mail']     = array('Bestätigung einer Mitgliederaktivierung', '');
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['member_registration_mail']   = array('Information einer Mitgliederregistrierung', '');
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['account_activation_mail']    = array('Aktivierung eines Accounts', '');
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['account_deactivation_mail']  = array('Deaktivierung eines Accounts', '');
