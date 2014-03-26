2780ed64a80cfafc3fc9f4310750327628aa11fd
David Maack <david.maack@arcor.de>
2014-03-26 18:27:33 +0100

    Language file added

---
+++ system/modules/registration_info_mailer/languages/de/tl_nc_notification.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @copyright  MEN AT WORK 2014
+ * @package    maxmind
+ * @license    GNU/LGPL
+ * @filesource
+ */
+
+
+/**
+ * Select lookup
+ */
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['rim']                        = 'RegistrationInfoMailer';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['member_activation_mail']     = array('Bestätigung einer Mitgliederregistrierung', '');
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['member_registration_mail']   = array('Information einer Mitgliederregistrierung', '');
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['account_activation_mail']    = array('Aktivierung eines Accounts', '');
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['account_deactivation_mail']  = array('Deaktivierung eines Accounts', '');
+
