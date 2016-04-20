fe81f077471c213a417bd71e39ba9763ed8f072d
Stefan Heimes <stefan_heimes@hotmail.com>
2016-04-20 10:31:46 +0200

    Add the new class names

--- system/modules/registration_info_mailer/dca/tl_member.php
+++ system/modules/registration_info_mailer/dca/tl_member.php
@@ -3,13 +3,19 @@
 /**
  * Contao Open Source CMS
  *
- * @copyright  MEN AT WORK 2014 
+ * @copyright  MEN AT WORK 2016
  * @package    registration_info_mailer
- * @license    GNU/LGPL 
+ * @license    GNU/LGPL
  * @filesource
  */
 
-$GLOBALS['TL_DCA']['tl_member']['palettes']['default'] = str_replace('login;', 'rim_send_mail,rim_activate_mailtemplate,rim_deactivate_mailtemplate,login;', $GLOBALS['TL_DCA']['tl_member']['palettes']['default']);
+// Add the new fields to the palette.
+$GLOBALS['TL_DCA']['tl_member']['palettes']['default'] = str_replace(
+    'login;',
+    'rim_send_mail,rim_activate_mailtemplate,rim_deactivate_mailtemplate,login;',
+    $GLOBALS['TL_DCA']['tl_member']['palettes']['default']
+);
+
 $GLOBALS['TL_DCA']['tl_member']['config']['onsubmit_callback'][] = array('registration_info_mailer', 'sendMemberMail');
 
 /**
@@ -19,37 +25,37 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['login']['eval']['tl_class'] .= ' clr'
 
 $GLOBALS['TL_DCA']['tl_member']['fields']['rim_send_mail'] = array
 (
-    'label'             => &$GLOBALS['TL_LANG']['tl_member']['rim_send_mail'],
-    'exclude'           => true,
-    'load_callback'     => array
+    'label'         => &$GLOBALS['TL_LANG']['tl_member']['rim_send_mail'],
+    'exclude'       => true,
+    'load_callback' => array
     (
-        array('RimHelper', 'resetSendCheckbox')
+        array('RegistrationInfoMailer\Helper', 'resetSendCheckbox')
     ),
-    'inputType'         => 'checkbox'
+    'inputType'     => 'checkbox'
 );
 
 $GLOBALS['TL_DCA']['tl_member']['fields']['rim_activate_mailtemplate'] = array
 (
-    'label'             => &$GLOBALS['TL_LANG']['tl_member']['rim_activate_mailtemplate'],
-    'exclude'           => true,
-    'inputType'         => 'select',
-    'options_callback'  => array('RimHelper', 'getAccountActivationTemplates'),
-    'load_callback'     => array
+    'label'            => &$GLOBALS['TL_LANG']['tl_member']['rim_activate_mailtemplate'],
+    'exclude'          => true,
+    'inputType'        => 'select',
+    'options_callback' => array('RegistrationInfoMailer\Helper', 'getAccountActivationTemplates'),
+    'load_callback'    => array
     (
-        array('RimHelper', 'setDefaultTemplate')
+        array('RegistrationInfoMailer\Helper', 'setDefaultTemplate')
     ),
-    'eval'              => array('tl_class'=>'w50')
+    'eval'             => array('tl_class' => 'w50')
 );
 
 $GLOBALS['TL_DCA']['tl_member']['fields']['rim_deactivate_mailtemplate'] = array
 (
-    'label'             => &$GLOBALS['TL_LANG']['tl_member']['rim_deactivate_mailtemplate'],
-    'exclude'           => true,
-    'inputType'         => 'select',
-    'options_callback'  => array('RimHelper', 'getAccountDeactivationTemplates'),
-    'load_callback'     => array
+    'label'            => &$GLOBALS['TL_LANG']['tl_member']['rim_deactivate_mailtemplate'],
+    'exclude'          => true,
+    'inputType'        => 'select',
+    'options_callback' => array('RegistrationInfoMailer\Helper', 'getAccountDeactivationTemplates'),
+    'load_callback'    => array
     (
-        array('RimHelper', 'setDefaultTemplate')
+        array('RegistrationInfoMailer\Helper', 'setDefaultTemplate')
     ),
-    'eval'              => array('tl_class'=>'w50')
+    'eval'             => array('tl_class' => 'w50')
 );
