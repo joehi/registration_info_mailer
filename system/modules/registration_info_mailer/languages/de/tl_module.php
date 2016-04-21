9a65b181bdde1243c3d7fbe1e97553ba06626029
Stefan Heimes <stefan_heimes@hotmail.com>
2016-04-21 12:43:18 +0200

    Update the language files
    - Add the new translations for the fields
    - Correct a failuer with wrong languages in the folders

--- system/modules/registration_info_mailer/languages/de/tl_module.php
+++ system/modules/registration_info_mailer/languages/de/tl_module.php
@@ -3,27 +3,61 @@
 /**
  * Contao Open Source CMS
  *
- * @copyright  MEN AT WORK 2014 
+ * @copyright  MEN AT WORK 2014
  * @package    registration_info_mailer
- * @license    GNU/LGPL 
+ * @license    GNU/LGPL
  * @filesource
  */
 
 /*
  * registration mails
  */
-$GLOBALS['TL_LANG']['tl_module']['rim_active']              = array('Registrierungsbenachichtigung aktivieren', 'Klicken Sie diese Schaltfläche an wenn Sie das Senden von Registrierungsinfos aktivieren wollen.');
-$GLOBALS['TL_LANG']['tl_module']['rim_mailtemplate']        = array('Mailtemplate', 'Wählen Sie hier das Template für die Mail aus. Im Template sind alle Standard-Insert-Tags verfügbar. Die Eigenschaften des Benutzers sind in den rim:: Insert Tags verfügbar (siehe Hilswizard). ');
-$GLOBALS['TL_LANG']['tl_module']['rim_do_syslog']           = array('Logging aktivieren', 'Durch Aktivieren dieser Option werden alle verschickten E-Mails im Systemlog vermerkt.');
+$GLOBALS['TL_LANG']['tl_module']['rim_active']       = array(
+    'Registrierungsbenachichtigung aktivieren',
+    'Klicken Sie diese Schaltfläche an wenn Sie das Senden von Registrierungsinfos aktivieren wollen.'
+);
+$GLOBALS['TL_LANG']['tl_module']['rim_mailtemplate'] = array(
+    'Mailtemplate',
+    'Wählen Sie hier das Template für die Mail aus. Im Template sind alle Standard-Insert-Tags verfügbar. Die Eigenschaften des Benutzers sind in den rim:: Insert Tags verfügbar (siehe Hilswizard). '
+);
+$GLOBALS['TL_LANG']['tl_module']['rim_do_syslog']    = array(
+    'Logging aktivieren',
+    'Durch Aktivieren dieser Option werden alle verschickten E-Mails im Systemlog vermerkt.'
+);
+
+/*
+ * change mails
+ */
+$GLOBALS['TL_LANG']['tl_module']['rim_change_active']       = array(
+    'Änderungsbenachichtigung aktivieren',
+    'Klicken Sie diese Schaltfläche an wenn Sie das Senden von Änderungsinfos aktivieren wollen.'
+);
+$GLOBALS['TL_LANG']['tl_module']['rim_change_mailtemplate'] = array(
+    'Mailtemplate',
+    'Wählen Sie hier das Template für die Mail aus. Im Template sind alle Standard-Insert-Tags verfügbar. Die Eigenschaften des Benutzers sind in den rim:: Insert Tags verfügbar (siehe Hilswizard). '
+);
+$GLOBALS['TL_LANG']['tl_module']['rim_change_do_syslog']    = array(
+    'Logging aktivieren',
+    'Durch Aktivieren dieser Option werden alle verschickten E-Mails im Systemlog vermerkt.'
+);
 
 /*
  * activation mails
  */
-$GLOBALS['TL_LANG']['tl_module']['rim_act_active']          = array('Aktivierungsbenachichtigung aktivieren', 'Klicken Sie diese Schaltfläche an wenn Sie das Senden von Aktivierungsinformationen aktivieren wollen.');
-$GLOBALS['TL_LANG']['tl_module']['rim_act_mailtemplate']    = array('Mailtemplate', 'Wählen Sie hier das Template für die Mail aus. Im Template sind alle Standard-Insert-Tags verfügbar. Die Eigenschaften des Benutzers sind in den rim:: Insert Tags verfügbar (siehe Hilswizard). ');
-$GLOBALS['TL_LANG']['tl_module']['rim_act_do_syslog']       = array('Logging aktivieren', 'Durch Aktivieren dieser Option werden alle verschickten E-Mails im Systemlog vermerkt.');
+$GLOBALS['TL_LANG']['tl_module']['rim_act_active']       = array(
+    'Aktivierungsbenachichtigung aktivieren',
+    'Klicken Sie diese Schaltfläche an wenn Sie das Senden von Aktivierungsinformationen aktivieren wollen.'
+);
+$GLOBALS['TL_LANG']['tl_module']['rim_act_mailtemplate'] = array(
+    'Mailtemplate',
+    'Wählen Sie hier das Template für die Mail aus. Im Template sind alle Standard-Insert-Tags verfügbar. Die Eigenschaften des Benutzers sind in den rim:: Insert Tags verfügbar (siehe Hilswizard). '
+);
+$GLOBALS['TL_LANG']['tl_module']['rim_act_do_syslog']    = array(
+    'Logging aktivieren',
+    'Durch Aktivieren dieser Option werden alle verschickten E-Mails im Systemlog vermerkt.'
+);
 
 /*
  * legend
  */
-$GLOBALS['TL_LANG']['tl_module']['rim_legend']              = 'Registration Info Mailer';
+$GLOBALS['TL_LANG']['tl_module']['rim_legend'] = 'Registration Info Mailer';
